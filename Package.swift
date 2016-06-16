e23c1700f4a7709bb4cc622c864813480ae449bd
Rene Hexel <r.hexel@griffith.edu.au>
2016-06-17 08:09:04 +1000

    Add Atk dependency

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ let package = Package(
     name: "Gtk",
     dependencies: [
         .Package(url: "https://github.com/rhx/CGtk.git", majorVersion: 1),
-        .Package(url: "https://github.com/rhx/SwiftGdk.git", majorVersion: 3)
+        .Package(url: "https://github.com/rhx/SwiftGdk.git", majorVersion: 3),
+        .Package(url: "https://github.com/rhx/SwiftAtk.git", majorVersion: 2)
     ]
 )
