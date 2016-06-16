e338c0d9c55c26d157340531a4f697aec3a9ff01
Rene Hexel <r.hexel@griffith.edu.au>
2016-06-17 07:55:28 +1000

    Keep convenience additions

--- distclean.sh
+++ distclean.sh
@@ -3,4 +3,4 @@
 # Remove Packages directory and generated files
 #
 . ./config.sh
-exec rm -rf Packages Sources/${Module}.swift Sources/Swift${Mod}.swift ${Mod}.xcodeproj
+exec rm -rf Packages Sources/${Module}.swift ${Mod}.xcodeproj
