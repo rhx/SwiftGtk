235ae5ac7c4a2bf385564f9af8f92413eb5b3379
Rene Hexel <r.hexel@griffith.edu.au>
2016-06-18 13:50:51 +1000

    Whitelist and test versioning functions

--- Tests/Gtk/GtkTests.swift
+++ Tests/Gtk/GtkTests.swift
@@ -1,23 +1,39 @@
 import XCTest
+import CGtk
 @testable import Gtk
 
 var app: Application!
+var appWasRunning = false
 
 class GtkTests: XCTestCase {
+    func testMajorVersion() { XCTAssertEqual(getMajorVersion(), gtk_get_major_version()) }
+    func testMinorVersion() { XCTAssertEqual(getMinorVersion(), gtk_get_minor_version()) }
+    func testMicroVersion() { XCTAssertEqual(getMicroVersion(), gtk_get_micro_version()) }
+    func testInterfaceAge() { XCTAssertEqual(getInterfaceAge(), gtk_get_interface_age()) }
+    func testBinaryAge()    { XCTAssertEqual(getBinaryAge(),    gtk_get_binary_age())    }
+
+    /// test that we can run and qui an app
     func testApp() {
         app = Application()
         XCTAssertNotNil(app)
         let status = app.run {
+            appWasRunning = true
             app.quit()
         }
         XCTAssertEqual(status, 0)
+        XCTAssertTrue(appWasRunning)
     }
 }
 
 extension GtkTests {
     static var allTests : [(String, (GtkTests) -> () throws -> Void)] {
         return [
-            ("testApp", testApp),
+            ("testApp",             testApp),
+            ("testMajorVersion",    testMajorVersion),
+            ("testMinorVersion",    testMinorVersion),
+            ("testMicroVersion",    testMicroVersion),
+            ("testInterfaceAge",    testInterfaceAge),
+            ("testBinaryAge",       testBinaryAge),
         ]
     }
 }
