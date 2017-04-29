d2ffe7ee55a62139c4e7279e864e20e678039634
Rene Hexel <r.hexel@griffith.edu.au>
2017-04-29 20:14:13 +1000

    Add drag source/dest convenience methods

--- Sources/DragContext.swift
+++ Sources/DragContext.swift
@@ -21,3 +21,84 @@ public extension DragContextProtocol {
         }
     }
 }
+
+
+public extension WidgetProtocol {
+    /// Set a drag source
+    ///
+    /// - Parameters:
+    ///   - startButton: button to start dragging from (defaults to `.button1_mask`)
+    ///   - action: drag action to perform (defaults to `.copy`)
+    ///   - targets: array of targets to target
+    public func dragSourceSet(startButton: Gdk.ModifierType = .button1_mask, action: Gdk.DragAction = .copy, targets: [String]) {
+        var t = targets.map { GtkTargetEntry(target: $0) }
+        dragSourceSet(startButtonMask: startButton, targets: &t, nTargets: CInt(t.count), actions: action)
+    }
+    /// Set a drag source
+    ///
+    /// - Parameters:
+    ///   - startButton: button to start dragging from (defaults to `.button1_mask`)
+    ///   - action: drag action to perform (defaults to `.copy`)
+    ///   - targets: array of targets to target
+    public func dragSourceSet(startButton: Gdk.ModifierType = .button1_mask, action: Gdk.DragAction = .copy, targets: [GtkTargetEntry]) {
+        var t = targets
+        dragSourceSet(startButtonMask: startButton, targets: &t, nTargets: CInt(t.count), actions: action)
+    }
+    /// Set a drag source
+    ///
+    /// - Parameters:
+    ///   - startButton: button to start dragging from (defaults to `.button1_mask`)
+    ///   - action: drag action to perform (defaults to `.copy`)
+    ///   - targets: list of targets to target
+    public func dragSourceSet(startButton b: Gdk.ModifierType = .button1_mask, action a: Gdk.DragAction = .copy, targets t: String...) {
+        dragSourceSet(startButton: b, action: a, targets: t)
+    }
+    /// Set a drag source
+    ///
+    /// - Parameters:
+    ///   - startButton: button to start dragging from (defaults to `.button1_mask`)
+    ///   - action: drag action to perform (defaults to `.copy`)
+    ///   - targets: list of targets to target
+    public func dragSourceSet(startButton b: Gdk.ModifierType = .button1_mask, action a: Gdk.DragAction = .copy, targets t: GtkTargetEntry...) {
+        dragSourceSet(startButton: b, action: a, targets: t)
+    }
+
+    /// Set a drag destination
+    ///
+    /// - Parameters:
+    ///   - flags: destination defaults (defaults to `.all`)
+    ///   - action: drag action to perform (defaults to `.copy`)
+    ///   - targets: array of targets to target
+    public func dragDestSet(flags f: DestDefaults = .all, action a: Gdk.DragAction = .copy, targets: [String]) {
+        var t = targets.map { GtkTargetEntry(target: $0) }
+        dragDestSet(flags: f, targets: &t, nTargets: CInt(t.count), actions: a)
+    }
+    /// Set a drag destination
+    ///
+    /// - Parameters:
+    ///   - flags: destination defaults (defaults to `.all`)
+    ///   - action: drag action to perform (defaults to `.copy`)
+    ///   - targets: array of targets to target
+    public func dragDestSet(flags f: DestDefaults = .all, action a: Gdk.DragAction = .copy, targets: [GtkTargetEntry]) {
+        var t = targets
+        dragDestSet(flags: f, targets: &t, nTargets: CInt(t.count), actions: a)
+    }
+    /// Set a drag destination
+    ///
+    /// - Parameters:
+    ///   - flags: destination defaults (defaults to `.all`)
+    ///   - action: drag action to perform (defaults to `.copy`)
+    ///   - targets: list of targets to target
+    public func dragDestSet(flags f: DestDefaults = .all, action a: Gdk.DragAction = .copy, targets t: String...) {
+        dragDestSet(flags: f, action: a, targets: t)
+    }
+    /// Set a drag destination
+    ///
+    /// - Parameters:
+    ///   - flags: destination defaults (defaults to `.all`)
+    ///   - action: drag action to perform (defaults to `.copy`)
+    ///   - targets: list of targets to target
+    public func dragDestSet(flags f: DestDefaults = .all, action a: Gdk.DragAction = .copy, targets t: GtkTargetEntry...) {
+        dragDestSet(flags: f, action: a, targets: t)
+    }
+}
