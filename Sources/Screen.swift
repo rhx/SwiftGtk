5082a5f879db1ad7a7e467a83ce6f86e8eb1f6a9
Rene Hexel <r.hexel@griffith.edu.au>
2017-04-30 13:19:50 +1000

    Call gtk_style_context_add_provider_for_screen() (not gtk_style_context_add_provider())

--- Sources/Screen.swift
+++ Sources/Screen.swift
@@ -5,20 +5,20 @@
 //  Created by Rene Hexel on 28/4/17.
 //  Copyright © 2017 Rene Hexel.  All rights reserved.
 //
+import CGdk
 import CGtk
 import Gdk
 
 public extension ScreenProtocol {
-    /// return the screen as a style context
-    public var styleContext: StyleContextRef { return StyleContextRef(cPointer: ptr) }
-
     /// Convenience method to add a style provider to the current screen
     ///
     /// - Parameters:
     ///   - p: style provider to add
     ///   - priority: the priority of the provider (defaults to `STYLE_PROVIDER_PRIORITY_APPLICATION`)
     public func add<P: StyleProviderProtocol>(provider p: P, priority: Int32 = STYLE_PROVIDER_PRIORITY_APPLICATION) {
-        return styleContext.add(provider: p, priority: CUnsignedInt(priority))
+        ptr.withMemoryRebound(to: GdkScreen.self, capacity: 1) { (s: UnsafeMutablePointer<GdkScreen>) -> Void in
+            gtk_style_context_add_provider_for_screen(s, p.ptr, guint(priority))
+        }
     }
 
     /// Convenience method to add a CSS style provider to the current screen
@@ -27,7 +27,8 @@ public extension ScreenProtocol {
     ///   - p: CSS style provider to add
     ///   - priority: the priority of the provider (defaults to `STYLE_PROVIDER_PRIORITY_APPLICATION`)
     public func add<P: CssProviderProtocol>(provider p: P, priority: Int32 = STYLE_PROVIDER_PRIORITY_APPLICATION) {
-        return styleContext.add(provider: p.styleProvider, priority: CUnsignedInt(priority))
+        ptr.withMemoryRebound(to: GdkScreen.self, capacity: 1) { (s: UnsafeMutablePointer<GdkScreen>) -> Void in
+            gtk_style_context_add_provider_for_screen(s, p.styleProvider.ptr, guint(priority))
+        }
     }
 }
-
