36af9575937d81b0b728fd1c240a70212628671f
Rene Hexel <r.hexel@griffith.edu.au>
2017-05-14 09:52:34 +1000

    Use parallel (if installed) to speed up post-processing

--- gir-to-swift.sh
+++ gir-to-swift.sh
@@ -22,8 +22,17 @@ if [ ! -e "${GIR}" ] ; then
 	exit 1
 fi
 gir2swift -o Sources -m ${Module}.module -p ${GIR_DIR}/GLib-2.0.gir -p ${GIR_DIR}/GObject-2.0.gir -p ${GIR_DIR}/Gio-2.0.gir -p ${GIR_DIR}/Pango-1.0.gir -p ${GIR_DIR}/cairo-1.0.gir -p ${GIR_DIR}/PangoCairo-1.0.gir -p ${GIR_DIR}/GdkPixbuf-2.0.gir -p ${GIR_DIR}/Atk-1.0.gir -p ${GIR_DIR}/Gdk-3.0.gir "${GIR}"
-for src in Sources/*-*.swift ; do
+if which parallel >/dev/null ; then
+    for src in Sources/*-*.swift ; do \
+	echo "sed -f ${Module}.sed < ${src} |"				\
+	     "awk -f ${Module}.awk > ${src}.out" \;			\
+	     "mv ${src}.out ${src}" ;					\
+    done | parallel
+else
+    for src in Sources/*-*.swift ; do
+	echo "Postprocessing Wrapper for `basename $src`"
 	sed -f ${Module}.sed < ${src} | awk -f ${Module}.awk > ${src}.out
 	mv ${src}.out ${src}
-done
+    done
+fi
 touch Sources/${Module}.swift
