4faae51fb347bbd54d2d0b496d4d6c8bb32cdbb9
Rene Hexel <r.hexel@griffith.edu.au>
2020-08-30 15:15:52 +1000

    Use local repository

--- Package.swift
+++ Package.swift
@@ -8,8 +8,10 @@ let package = Package(
         .library(name: "Gtk", targets: ["Gtk"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/rhx/SwiftAtk.git", .branch("master")),
-        .package(url: "https://github.com/rhx/SwiftGdk.git", .branch("master")),
+        .package(url: "file:///Users/rh/src/swift/rh/gtk/SwiftAtk", .branch("master")),
+        .package(url: "file:///Users/rh/src/swift/rh/gtk/SwiftGdk", .branch("master")),
+        //.package(url: "https://github.com/rhx/SwiftAtk.git", .branch("master")),
+        //.package(url: "https://github.com/rhx/SwiftGdk.git", .branch("master")),
     ],
     targets: [
 	.systemLibrary(name: "CGtk", pkgConfig: "gtk-3.0 gdk-3.0 pangocairo pangoft2 pango gio-unix-2.0 glib-2.0",
