0daa75a7b1233e5773453230c09197b512141100
Mikoláš Stuchlík <stuchlej@gmail.com>
2020-10-20 01:57:22 +0200

    Remove the need to use build sh

--- Package.swift
+++ Package.swift
@@ -6,11 +6,11 @@ let package = Package(
     name: "Gtk",
     products: [ .library(name: "Gtk", targets: ["Gtk"]) ],
     dependencies: [
-        .package(name: "Atk", url: "https://github.com/rhx/SwiftAtk.git", .branch("master")),
-        .package(name: "Gdk", url: "https://github.com/rhx/SwiftGdk.git", .branch("master")),
+        .package(name: "Atk", url: "https://github.com/mikolasstuchlik/SwiftAtk.git", .branch("master")),
+        .package(name: "Gdk", url: "https://github.com/mikolasstuchlik/SwiftGdk.git", .branch("master")),
     ],
     targets: [
-	.systemLibrary(name: "CGtk", pkgConfig: "gtk-3.0 gdk-3.0 pangocairo pangoft2 pango gio-unix-2.0 glib-2.0",
+	.systemLibrary(name: "CGtk", pkgConfig: "gtk+-3.0",
 	    providers: [
 		.brew(["gtk+3", "glib", "glib-networking", "gobject-introspection"]),
 		.apt(["libgtk-3-dev", "libglib2.0-dev", "glib-networking", "gobject-introspection", "libgirepository1.0-dev"])
