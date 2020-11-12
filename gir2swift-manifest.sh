6692d2404d970ef1f18a52a3bc6c0415a1a1a0b9
Mikoláš Stuchlík <stuchlej@gmail.com>
2020-11-12 14:07:14 +0100

    WIP: Change build scripts

--- gir2swift-manifest.sh
+++ gir2swift-manifest.sh
@@ -2,11 +2,27 @@
 
 GIR_NAME="Gtk-3.0"
 
-function generate {
-    echo "building"
+function generate_arg-path_arg-g2s-exec_arg-gir-pre_arg-gir-path {
+    local PACKAGE_PATH=$1
+    local G2S_EXEC=$2
+    local GIR_PRE=$3
+    local GIR_PATH=$4
+
+    local CALLER=$PWD
+
+    cd $PACKAGE_PATH
+
+    local NAME=$(package_name)
+    local GIR_PRE_ARGS=`for FILE in ${GIR_PRE}; do echo -n "-p ${GIR_PATH}/${FILE}.gir "; done`
+    
+    bash -c "${G2S_EXEC} -o Sources/${NAME} -s -m ${GIR_NAME}.module ${GIR_PATH}/${GIR_NAME}.gir ${GIR_PRE_ARGS}"
+
+    ...
+
+    cd $CALLER
 }
 
 case $1 in
 gir-name) echo $GIR_NAME;;
-generate) echo $(generate);;
+generate) echo $(generate_arg-path_arg-g2s-exec_arg-gir-pre_arg-gir-path "$2" "$3" "$4" "$5");;
 esac
