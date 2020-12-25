7cebb529b57f8359c3f50ca781562e3f288b0378
Mikoláš Stuchlík <stuchlej@gmail.com>
2020-12-25 18:49:46 +0100

    Improve rung2s

--- run-gir2swift.sh
+++ run-gir2swift.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 swift package update
-.build/checkouts/gir2swift/gir2swift-generation-driver.sh $PWD
+
+case $1 in
+flags) .build/checkouts/gir2swift/gir2swift-generation-driver.sh c-flags $PWD ;;
+clean) .build/checkouts/gir2swift/gir2swift-generation-driver.sh remove-generated $PWD ;;
+*) .build/checkouts/gir2swift/gir2swift-generation-driver.sh generate $PWD ;;
+esac
