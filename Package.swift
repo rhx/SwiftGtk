183f7b69dad08478652ed980cd21ecfaf5461500
Rene Hexel <r.hexel@griffith.edu.au>
2021-01-10 14:27:05 +1000

    Add new build system

--- Package.swift
+++ Package.swift
@@ -6,16 +6,21 @@ let package = Package(
     name: "Gtk",
     products: [ .library(name: "Gtk", targets: ["Gtk"]) ],
     dependencies: [
-	.package(name: "Gdk", url: "https://github.com/rhx/SwiftGdk.git", .branch("gtk4")),
-	.package(name: "PangoCairo", url: "https://github.com/rhx/SwiftPangoCairo.git", .branch("main")),
+        .package(name: "gir2swift", url: "https://github.com/rhx/gir2swift.git", .branch("development")),
+        .package(name: "Gdk", url: "https://github.com/rhx/SwiftGdk.git", .branch("gtk4-development")),
+        .package(name: "PangoCairo", url: "https://github.com/rhx/SwiftPangoCairo.git", .branch("development")),
     ],
     targets: [
-	.systemLibrary(name: "CGtk", pkgConfig: "gtk4",
+        .systemLibrary(name: "CGtk", pkgConfig: "gtk4",
 	    providers: [
-		.brew(["gtk+4", "glib", "glib-networking", "gobject-introspection"]),
+		.brew(["gtk4", "glib", "glib-networking", "gobject-introspection"]),
 		.apt(["libgtk-4-dev", "libglib2.0-dev", "glib-networking", "gobject-introspection", "libgirepository1.0-dev"])
 	    ]),
-        .target(name: "Gtk", dependencies: ["GtkCHelpers", "Gdk", "PangoCairo"]),
+        .target(
+            name: "Gtk", 
+            dependencies: ["GtkCHelpers", "Gdk", "PangoCairo"],
+            swiftSettings: [.unsafeFlags(["-Xfrontend", "-serialize-debugging-options"], .when(configuration: .debug))]
+        ),
         .target(name: "GtkCHelpers", dependencies: ["CGtk"]),
         .testTarget(name: "GtkTests", dependencies: ["Gtk"]),
     ]
