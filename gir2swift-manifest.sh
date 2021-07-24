478d3588abda1a7d4f776f4ea0c2e5d9e4a5d450
Rene Hexel <r.hexel@griffith.edu.au>
2021-07-24 16:19:04 +1000

    Simplify manifest: let gir2swift do sed/awk post-processing

--- gir2swift-manifest.sh
+++ gir2swift-manifest.sh
@@ -16,26 +16,7 @@ function generate_arg-path_arg-g2s-exec_arg-gir-pre_arg-gir-path {
     local PKG_CONFIG_ARG=$(package_pkg_config_arguments)
     local GIR_PRE_ARGS=`for FILE in ${GIR_PRE}; do echo -n "-p ${GIR_PATH}/${FILE}.gir "; done`
     
-    bash -c "${G2S_EXEC} -o Sources/${NAME} -m ${GIR_NAME}.module ${GIR_PRE_ARGS} ${GIR_PATH}/${GIR_NAME}.gir"
-
-    if which parallel >/dev/null ; then
-        for src in Sources/${NAME}/*-*.swift ; do \
-	    echo "sed -f ${GIR_NAME}.sed < ${src} |"				\
-	         "awk -f ${GIR_NAME}.awk > ${src}.out" \;			\
-	         "mv ${src}.out ${src}" ;					\
-        done | parallel
-    else
-        for src in Sources/${NAME}/*-*.swift ; do
-	    echo "Postprocessing Wrapper for `basename $src`"
-	    sed -f ${GIR_NAME}.sed < ${src} | awk -f ${GIR_NAME}.awk > ${src}.out
-	    mv ${src}.out ${src}
-        done
-    fi
-    if ! pkg-config --atleast-version=3.24 $PKG_CONFIG_ARG ; then
-	    rm -f Sources/${NAME}/EventControllerSignal.swift
-	    rm -f Sources/${NAME}/GestureSignal.swift
-    fi
-    touch Sources/${NAME}/${GIR_NAME}.swift
+    bash -c "${G2S_EXEC} -o Sources/${NAME} ${GIR_PRE_ARGS} ${GIR_PATH}/${GIR_NAME}.gir"
 
     cd $CALLER
 }
