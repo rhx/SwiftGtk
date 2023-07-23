e6666c84b084d954aec98bec97b0bee14d7035ea
Rene Hexel <r.hexel@griffith.edu.au>
2023-07-23 19:02:17 +1000

    Make CGtk and CGsk build on Ubuntu/aarch64.

--- Sources/CGsk/gsk_bridging.h
+++ Sources/CGsk/gsk_bridging.h
@@ -45,6 +45,7 @@ typedef struct _GskGLShader GskGLShader;
 typedef struct _GskNglRenderer GskNglRenderer;
 
 #include <sys/types.h>
+#include <stdint.h>
 #include <gsk/gsk.h>
 #if __has_include(<gsk/gl/gsknglrenderer.h>)
 #include <gsk/gl/gsknglrenderer.h>
