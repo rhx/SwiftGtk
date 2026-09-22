d2b94c73a4b65794b5be6b13c9233442374aa4bb
Rene Hexel <r.hexel@griffith.edu.au>
2026-09-22 11:51:32 +1000

    Test headless Gdk app launch context registration

--- Tests/GdkTests/GdkTests.swift
+++ Tests/GdkTests/GdkTests.swift
@@ -1,10 +1,14 @@
 import XCTest
+import CGdk
 @testable import Gdk
 
 class GdkTests: XCTestCase {
 
-    // FIXME: implement gdk-4 tests
-    func testEmpty() {
+    /// Checks that GDK registers its app launch context type.
+    ///
+    /// Type registration does not require a display connection.
+    func testAppLaunchContextType() {
+        XCTAssertNotEqual(gdk_app_launch_context_get_type(), .invalid)
     }
 
 }
